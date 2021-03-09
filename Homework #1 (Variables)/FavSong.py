c783edd99063ccc0417630290116f3f8ab9d0565
SucharitaDhar <64468430+SucharitaDhar@users.noreply.github.com>
2021-03-09 15:32:55 +0530

    Homework #1 (Variables)
    Homework #1 (Variables)

---
+++ Homework #1 (Variables)/FavSong.py
@@ -0,0 +1,31 @@
+#My favorite song described
+
+#Genre, Artists and Album
+
+
+Genre = "Bollywood Romance"
+Composer = "Amit Trivedi"
+Lyricist = "Amitabh Bhattacharya" 
+Singer_male = "Arijit Singh"  
+Singer_female = "Nikhita Gandhi"
+Album = "Kedarnath"
+
+#YearReleased
+
+YearReleased = 2018
+
+#Duration
+
+DurationInSeconds = 351
+
+print("My favorite song is from the Album " + Album + " and in the genre " + Genre)
+print("The artists of this song are :")
+print("Composer: " + Composer)
+print("Lyricist: " + Lyricist)
+print("Singer(Male): " + Singer_male)
+print("Singer(Female): " + Singer_female)
+
+print("It was released in the year" , YearReleased , "and has a duration of", DurationInSeconds , "seconds")
+
+
+
